91b8bb450407ebfc3008497df67e583a6d5a26ae
harikrushna0 <harikrushnasuhagiya59@gmail.com>
2025-11-29 16:34:20 +0530

    feat: implement user CRUD operations and update signup endpoint

--- src/middlewares/app.js
+++ src/middlewares/app.js
@@ -1,14 +1,77 @@
 const express = require("express");
 const connectDB = require("../config/database");
 const UserSchema = require("../model/user");
+const { tr } = require("@faker-js/faker");
 const app = express();
 
 //run on every request
 //use middleware to parse json body data
 app.use(express.json());
 
-app.post("/signup", async (req, res) => {
+//get one user by emailid
+app.get("/user",async(req,res)=>{
+  const emailId=req.body.emailId;
+  try{
+    const user=await UserSchema.findOne({emailId:emailId});
+    if(user){
+      res.status(200).json({message:"user fetched successfully",data:user});
+    }else{
+      res.status(404).json({message:"user not found"});
+    }
+  }
+  catch(err){
+    res.status(500).json({message:"error while fetching user",data:err.message});
+  }
+});
+
+//feed api -get all the user on page loads on first time when we login in devTinder
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await UserSchema.find({});
+    res
+      .status(200)
+      .json({ message: "users fetched successfully", data: users });
+  } catch (err) {
+    res.status(404).json({ message: "users not found", data: err.message });
+  }
+});
+
+// delete api
+app.delete("/user",async(req,res)=>{
+  const userId = req.query.userId;
+  try{
+    const deletedUser= await UserSchema.findByIdAndDelete(userId);
+    if(deletedUser){
+      res.status(200).json({message:"user deleted successfully",data:deletedUser});
+    }else{
+      res.status(404).json({message:"user not found"});
+    }
+  }
+  catch(err){
+    res.status(500).json({message:"error while deleting user",data:err.message});
+  }
+})
+
+//update api
+app.patch("/user",async(req,res)=>{
+
+  const emailId= req.body.emailId;
+  const data = req.body;
+
+  try{
+    await UserSchema.findOneAndUpdate({emailId:emailId},data);
+    res.status(200).json({message:"user updated successfully"});
+  }
+  catch(err){
+    res.status(500).json({message:"error while updating user",data:err.message});
+  }
   
+  
+
+});
+
+//signup api
+app.post("/signup", async (req, res) => {
   //creating a new instance of the user model
   const user = new UserSchema(req.body);
   try {
